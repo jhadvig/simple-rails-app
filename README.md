a0c42fe46eaa0127719099f5f7caa0e2c49a858f
jhadvig <jhadvig@redhat.com>
2014-04-24 15:54:44 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-simple-rails-app
-================
+# Sample Rails App for OpenShift STI #
+Rails application for openshift STI.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleRailsApp::Application.config.secret_token = '10b85e733e49fc1e0f1d67ae084776a723da0dd4e9ae15385f7ca6de4283fd4aaf416442f930c92ab6eb5bd6f7141d9c1d017572df2665b165c80f1923868446'
